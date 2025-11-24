d761ea47a8431814caa2fc4929cf71a5b038fee3
JiXinyi02 <310932236@qq.com>
2025-11-24 19:56:25 +0800

    Question 1 solved

--- hw2/Problem_1.jl
+++ hw2/Problem_1.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra
 using SparseArrays
+using Statistics
 using Random
 using Plots
 using PyPlot
@@ -58,30 +59,105 @@ function generate_data(n; alpha=5.0, seed=5)
 end
 
 # Visualization using semilogy
-function semilogy_plot(error_list, residual_list, x_exact; title_str="GMRES Convergence")
+function semilogy_plot(error_list, residual_list, theory_list; title_str="GMRES Convergence_Localization Disk")
     iterations = 1:length(error_list)
     
     figure()
     semilogy(iterations, error_list, color="black", linestyle="--", label="Relative Error")
     semilogy(iterations, residual_list, color= "red", linestyle="-", label="Relative Residual")
-    semilogy(iterations, x_exact, color= "blue", linestyle= "--", label="theory prediction")
+    semilogy(iterations, theory_list, color= "blue", linestyle= "--", label="theory prediction by Localization Disk")
 
     xlabel("Iteration")
     ylabel("Error")
 
-    grid=true
+    PyPlot.grid(true)
     title(title_str)
     legend()
-    PyPlot.savefig("gmres_convergence.png")
+    PyPlot.savefig(title_str * ".png")
 
 end
 
+# To calculate the Gershgorin disks and plot eigenvalues and disks
+function ger_disk(A)
+    c = tr(A) / size(A, 1)
+    # We choose to use the outlier version of Gershgorin disks
+    R1 = sort([sum(abs.(A[i, :])) - abs(A[i, i]) for i in 1:size(A, 1)])
+    R = quantile(R1, 0.9)
+    return c, R
+end
+
+function loc_disk(A)
+    λ = eigvals(Matrix(A))
+    centroid = sum(λ) / length(λ)
+    distances = sort(abs.(λ .- centroid))
+    radius = distances[end-1] #Second largest to consider the outlier
+    return centroid, radius
+end
+
+function plot_ger_disk(A, a)
+    λ = eigvals(Matrix(A))
+    c, R = loc_disk(A)
+
+    θ = LinRange(0, 2π, 100)
+    x_circle = real(c) .+ R * cos.(θ)
+    y_circle = imag(c) .+ R * sin.(θ)
+
+    Plots.scatter(real.(λ), imag.(λ), color=:black, marker = :cross, label="Eigenvalues")
+    Plots.plot!(x_circle, y_circle, color=:red, label="Localization Disk", xlabel="Re λ", ylabel="Im λ", title="α = $(a)")
+    xlims!(-0.2, 1.2)
+    ylims!(-0.2, 0.2)
+
+    Plots.savefig("eigenvalues_α=$(a).png")
+end
+
+function gmres_measure(A, b, m)
+    t = @elapsed x = GMRES(A, b, nmax=m)
+    resnorm = norm(A*x - b)
+    return t, resnorm
+end
 
 function main()
-    A, b = generate_data(100, alpha=5.0, seed=5)
-    x_exact = A \ b
-    x_computed, error_list, residual_list = GMRES(A, b, tol=1e-20, nmax=100, x0=zeros(length(b)), x_exact=x_exact)
-    semilogy_plot(error_list, residual_list, x_exact, title_str="GMRES Convergence for n=100")
+    alpha = [1.0, 5.0, 10.0, 100.0]
+    nmax = 100
+    for a in alpha
+        A, b = generate_data(100, alpha=a, seed=5)
+        x_exact = A \ b
+        # question(a): GMRES convergence plot
+        x_computed, error_list, residual_list = GMRES(A, b, tol=1e-20, nmax=100, x0=zeros(length(b)), x_exact=x_exact)
+        c, R = loc_disk(A)
+        ρ = R / abs(c)
+        theory_list = ρ .^ (1:nmax)
+        semilogy_plot(error_list, residual_list, theory_list, title_str="Localization Disk_GMRES Convergence for α=$(a)")
+        
+        # question(b): Plot the eigenvalues of A for different α
+        plot_ger_disk(A, a)
+    end
+
+    # # question(c): Time and residual norm measurement
+    # Attention: If we want to run this part, we need to change the return value of GMRES to return only xk.
+    
+    # alphas = [1.0, 100.0]
+    # n_list = [200, 500, 1000]
+    # m_list = [5, 10, 20, 50, 100]
+    # for a in alphas
+    #     println("Alpha = $(a)")
+    #     for n in n_list
+    #         A, b = generate_data(n, alpha=a, seed=5)
+    #         for m in m_list
+    #             # warm-up
+    #             x = GMRES(A, b, nmax=m)
+    #             t, resnorm = gmres_measure(A, b, m)
+    #             println("n=$(n), m=$(m): time=$(t), residual norm=$(resnorm)")
+    #         end
+
+    #         # backslash
+    #         t_bs = @elapsed x_bs = A \ b
+    #         resnorm_bs = norm(A*x_bs - b)
+    #         println("n=$(n), backslash: time=$(t_bs), residual norm=$(resnorm_bs)")
+    #     end
+    # end
+
+    
 end
 
 main()
